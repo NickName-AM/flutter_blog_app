a86b4e091a67bdde63384666edcb08510a9b9145
NickName-AM <maharjanebik061@gmail.com>
2025-08-04 17:53:54 +0545

    signup page

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/theme/app_palette.dart';
+
+class AppTheme{
+
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 3,
+          color: color,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+    ),
+  );
+}
---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/theme/app_palette.dart';
+import 'package:flutter_blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:flutter_blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up.',
+              style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 30,),
+            AuthField(hintText: 'Name'),
+            const SizedBox(height: 15,),
+            AuthField(hintText: 'Email'),
+            const SizedBox(height: 15,),
+            AuthField(hintText: 'Password'),
+            const SizedBox(height: 20,),
+            AuthGradientButton(),
+            const SizedBox(height: 20,),
+            RichText(text: TextSpan(
+              text: "Don't have an account? ",
+              style: Theme.of(context).textTheme.titleMedium,
+              children: [
+                TextSpan(
+                  text: 'Sign In',
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: AppPallete.gradient2,
+                    fontWeight: FontWeight.bold,
+                  ),
+
+                )
+              ]
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
