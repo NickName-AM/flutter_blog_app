47ca34177bf8b587f9e4007e33a1baa035b54339
NickName-AM <maharjanebik061@gmail.com>
2025-08-06 10:45:16 +0545

    appBar: signup_page

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -18,5 +18,8 @@ class AppTheme{
       enabledBorder: _border(),
       focusedBorder: _border(AppPallete.gradient2),
     ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppPallete.backgroundColor,
+    ),
   );
 }
